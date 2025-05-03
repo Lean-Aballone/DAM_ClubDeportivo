ba55122b2563239111cbed1f1c2235f792c69c66
Lean-Aballone <l.aballone@gmail.com>
2025-05-02 22:28:34 -0300

    Added sectionMain activity

--- app/src/main/java/com/example/clubdeportivo/sectionMain.kt
+++ app/src/main/java/com/example/clubdeportivo/sectionMain.kt
@@ -1,23 +1,16 @@
 package com.example.clubdeportivo
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class sectionMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, sectionMain::class.java)
-            startActivity(intent)
-        }
+        setContentView(R.layout.activity_section_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
