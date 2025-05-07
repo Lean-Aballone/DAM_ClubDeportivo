6c748ef9f405f4b7804c64b389ea1a1a4b542604
Lean-Aballone <l.aballone@gmail.com>
2025-05-07 17:08:02 -0300

    Added returnToMain functionality

--- app/src/main/java/com/example/clubdeportivo/sectionInscripcion.kt
+++ app/src/main/java/com/example/clubdeportivo/sectionInscripcion.kt
@@ -1,9 +1,11 @@
 package com.example.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.view.View
 import androidx.core.view.WindowInsetsCompat
 
 class sectionInscripcion : AppCompatActivity() {
@@ -16,5 +18,9 @@ class sectionInscripcion : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun returnToMain(view: View){
+        val intent = Intent(this, sectionMain::class.java)
+        startActivity(intent)
     }
 }
