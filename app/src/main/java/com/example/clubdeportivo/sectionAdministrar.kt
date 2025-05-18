e2e959df7ff8bf8f4cffc0bca2b4a0dd71efe307
Lean-Aballone <l.aballone@gmail.com>
2025-05-17 21:14:45 -0300

    Added Spinner functionality

--- app/src/main/java/com/example/clubdeportivo/sectionAdministrar.kt
+++ app/src/main/java/com/example/clubdeportivo/sectionAdministrar.kt
@@ -1,6 +1,13 @@
 package com.example.clubdeportivo
 
+import android.content.Intent
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +23,36 @@ class sectionAdministrar : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val spinner: Spinner = findViewById(R.id.spinner)
+        val listItems = listOf("DNI", "test")
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.spinner_item_closed,
+            listItems
+        )
+
+
+        adapter.setDropDownViewResource(R.layout.spinner_item_dropdown)
+        spinner.adapter = adapter
+        val button = findViewById<Button>(R.id.button)
+        button.post {
+            val width = button.paint.measureText(button.text.toString()) + button.paddingStart.toFloat()
+            val textShader = LinearGradient(
+                0f, 0f, width, 0f,
+                intArrayOf(
+                    0xFF00FFFF.toInt(), // #0FF
+                    0xFFFF00FF.toInt() // #F0F
+                ),
+                null,
+                Shader.TileMode.CLAMP
+            )
+            button.paint.shader = textShader
+            button.invalidate()
+        }
     }
+    fun returnToMain(view: View){
+        val intent = Intent(this, sectionMain::class.java)
+        startActivity(intent)
+    }
+
 }
