6eec5cf1a5644547fb80a36366b376b6ae1164a3
Lean-Aballone <l.aballone@gmail.com>
2025-05-28 14:57:05 -0300

    LoginHelper implemented in MainActivity

--- app/src/main/java/com/example/clubdeportivo/MainActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/MainActivity.kt
@@ -5,21 +5,38 @@ import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dbHelper: LoginHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.login)
+        dbHelper = LoginHelper(this)
         val button = findViewById<Button>(R.id.buttonSignIn)
+        val user = findViewById<EditText>(R.id.editTextText)
+        val pass = findViewById<EditText>(R.id.editTextTextPassword)
         button.setOnClickListener {
-            val intent = Intent(this, sectionMain::class.java)
-            startActivity(intent)
+            if (dbHelper.isValidUser(user.text.toString().trim(), pass.text.toString().trim())) {
+                Toast.makeText(
+                    this,
+                    "Bienvenido: " + user.text.toString().trim(),
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(this, sectionMain::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
