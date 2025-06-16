542654ecb7bbea7d8f306518d5e61f1d66753be7
Cecilia Fontenla <ceciliafontenla@gmail.com>
2025-06-15 21:41:15 -0300

    Datos dinamicos del cliente enviado desde la activity anterior

--- app/src/main/java/com/example/clubdeportivo/DetalleCliente.kt
+++ app/src/main/java/com/example/clubdeportivo/DetalleCliente.kt
@@ -3,13 +3,22 @@ package com.example.clubdeportivo
 import android.content.Intent
 import android.graphics.LinearGradient
 import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.clubdeportivo.entidades.Cliente
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DetalleCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +30,43 @@ class DetalleCliente : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val cliente: Cliente? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getSerializableExtra("cliente", Cliente::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getSerializableExtra("cliente") as? Cliente
+        }
+
+
+        cliente?.let {
+            Log.d("test Cliente", "Nombre: ${it.nombre}, DNI: ${it.dni}")
+        }
+
+
+        val nombre = findViewById<TextView>(R.id.nombreApellido)
+        val dni = findViewById<TextView>(R.id.dni)
+        val fechaInscripcion = findViewById<TextView>(R.id.fechaInscripcion)
+        val telefono = findViewById<TextView>(R.id.telefono)
+        val direccion = findViewById<TextView>(R.id.direccion)
+        val socio = findViewById<TextView>(R.id.socio)
+
+        if (cliente != null) {
+            socio.text = if(cliente.socio) { "Socio ID:" + cliente.id.toString() } else {"Cliente ID: " + cliente.id.toString()}
+            nombre.text = cliente.nombre + " " + cliente.apellido
+            dni.text = "DNI:" + cliente.dni.toString()
+            telefono.text = "Teléfono: " + cliente.telefono
+            direccion.text = "Dirección: " + cliente.direccion
+            Log.d("test", cliente.fechaInscripcion.toString())
+            val formato = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+            val fechaFormateada = formato.format(cliente.fechaInscripcion)
+            fechaInscripcion.text = fechaFormateada
+        } else {
+            Toast.makeText(this, "Ha ocurrido un error. Busque el cliente por DNI o Id Socio", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, sectionAdministrar::class.java)
+            startActivity(intent)
+        }
+
         val buttons = arrayOf(
             findViewById<Button>(R.id.imprimirCarnet),
             findViewById<Button>(R.id.registarPago),
