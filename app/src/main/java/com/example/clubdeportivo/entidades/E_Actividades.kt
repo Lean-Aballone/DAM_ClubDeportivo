e62e53e6e15ce3cb4dcc1a39c1c3ae5d259d11be
Lean-Aballone <l.aballone@gmail.com>
2025-06-18 19:23:29 -0300

    Created data class E_Actividades

---
+++ app/src/main/java/com/example/clubdeportivo/entidades/E_Actividades.kt
@@ -0,0 +1,10 @@
+package com.example.clubdeportivo.entidades
+
+import com.example.clubdeportivo.Deporte
+
+data class E_Actividades (
+    val deporte: Deporte,
+    val dias: Array<String>,
+    val HorarioInicio: String,
+    val HorarioFin: String
+)
