97d606f5db19ccb0abee95b1b1490b798058bcc7
Lean-Aballone <l.aballone@gmail.com>
2025-06-18 19:42:25 -0300

    E_Actividades, Added Id

--- app/src/main/java/com/example/clubdeportivo/entidades/E_Actividades.kt
+++ app/src/main/java/com/example/clubdeportivo/entidades/E_Actividades.kt
@@ -3,6 +3,7 @@ package com.example.clubdeportivo.entidades
 import com.example.clubdeportivo.Deporte
 
 data class E_Actividades (
+    val id: Int,
     val deporte: Deporte,
     val dias: Array<String>,
     val HorarioInicio: String,
